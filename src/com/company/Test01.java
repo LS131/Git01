e111fc39cf29eebc2a68a6feed2a78e407927c28
LSL131 <2135711440@qq.com>
2022-09-22 14:00:01 +0800

    第一次提交数据

---
+++ src/com/company/Test01.java
@@ -0,0 +1,17 @@
+package com.company;
+
+/**
+ * @author ls
+ * @date 2022/9/22 - 13:55
+ * 点击了add后就会变为绿色。说明添加到了暂存区
+ *
+ *
+ *
+ *
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("Test01");
+    }
+
+}

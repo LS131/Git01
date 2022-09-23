309029fdb48999869ab5b7fb8f39d1636f37c510
LS131 <70054474+LS131@users.noreply.github.com>
2022-09-23 08:01:41 +0800

    谁便

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+	System.out.println("one");
+	    System.out.println("thwo");
+	    System.out.println("two");
+	     System.out.println("thwo");
     }
 }

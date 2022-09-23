5081594bd0793e3a0b0db9826a5be8056c13dd75
LSL131 <2135711440@qq.com>
2022-09-23 08:21:02 +0800

    有鬼啊

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,12 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("sfsfsfsf");
-        System.out.println("sfsfsfsf");
-	    System.out.println("thwo");
-	    System.out.println("two");
-	    System.out.println("thwo");
+        System.out.println("我是你爸爸");
+        System.out.println("two");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+
+
     }
 }
